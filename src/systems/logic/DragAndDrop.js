908985afeab9733ecd38192a2deb930982b710a1
Anzor <slevin-82@mail.ru>
2025-08-16 12:51:18 +0300

    add

--- src/systems/logic/DragAndDrop.js
+++ src/systems/logic/DragAndDrop.js
@@ -161,7 +161,7 @@ export class DragAndDrop {
     
     const target = this.getDropTarget(fromPoint);
     const marker = document.createElement("div");
-    marker.textContent = `${target.textContent}`;
+    marker.textContent = `${target.textContent || target}`;
     marker.style.cssText = `
         position: fixed;
         left: ${event.clientX}px;
