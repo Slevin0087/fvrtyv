3b7cc6e655edb93d78eff707c278f2c901df9261
Anzor <slevin-82@mail.ru>
2025-10-08 15:16:12 +0300

    modified

--- src/ui/UIPlayerStatePage.js
+++ src/ui/UIPlayerStatePage.js
@@ -23,39 +23,50 @@ export class UIPlayerStatePage extends UIPage {
     const container = document.getElementById("player-state-content");
     container.innerHTML = "";
     container.innerHTML = `<dl class="p-state-table table">
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_name">Имя:</dt>
         <dd class="right-td">${playerName}</dd>
-
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_coins">Хусынки:</dt>
         <dd class="right-td">${statePlayer.coins}</dd>
-  
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_games_played">Сыграно игр:</dt>
         <dd class="right-td">${storagePlayer.gamesPlayed}</dd>
-  
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_games_won">Выиграно игр:</dt>
         <dd class="right-td">${statePlayer.wins}</dd>
-        
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_games_won_no_undo">Без отмен ходов:</dt>
         <dd class="right-td">${storagePlayer.winsWithoutUndo}</dd>
-
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_games_won_no_hints">Без подсказок:</dt>
         <dd class="right-td">${storagePlayer.winsWithoutHints}</dd>
-
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_best_score">Лучший счет:</dt>
         <dd class="right-td">${statePlayer.highestScore}</dd>
- 
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_best_time">Лучшее время:</dt>
         <dd class="right-td">${"00:00"}</dd>
-  
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_moves">Ходов за игру:</dt>
         <dd class="right-td">${stateGame.moves}</dd>
-  
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="all_player_state_moves">Ходов за всё время:</dt>
         <dd class="right-td">${storagePlayer.totalMoves}</dd>
-
+      </div>
+      <div class='wrap-line'>
         <dt class="left-td" data-i18n="player_state_achievement">Достижение:</dt>
         <dd class="right-td">${statePlayer.achievements.active.icon}</dd>
-   
+      </div>
     </dl>`;
   }
 
